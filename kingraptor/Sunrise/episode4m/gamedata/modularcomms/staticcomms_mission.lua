15167ed39293688fc2a3ba63b4d9cf81ef693b05
L.J. Lim <kingraptorowns@gmail.com>
2016-05-21 21:54:41 +0800

    Sunrise 4M fixes/improvements * Add some enemy patrols * Remove Defenders from all hills immediately surrounding base * Add one extra solar at start * Move storage further out where it won't be burned at start * Update commander handling

---
+++ kingraptor/Sunrise/episode4m/gamedata/modularcomms/staticcomms_mission.lua
@@ -0,0 +1,13 @@
+local comms = {
+  comm_campaign_ada = {
+    chassis = "cremcom3",
+    modules = { "commweapon_beamlaser", "commweapon_rocketlauncher", "module_ablative_armor", "module_autorepair", "module_high_power_servos"},
+  },
+  
+  comm_campaign_freemachine = {
+    chassis = "commstrike3",
+    modules = { "commweapon_riotcannon", "commweapon_heatray", "module_ablative_armor", "module_ablative_armor", "module_adv_targeting", "module_autorepair" },
+  },
+}
+
+return comms
