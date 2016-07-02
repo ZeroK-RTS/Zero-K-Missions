c2b4dc1282d5af56ebd62748835db1042823f0b3
L.J. Lim <kingraptorowns@gmail.com>
2016-07-02 23:07:42 +0800

    Tutorial 2: externalise strings; campaign version

---
+++ kingraptor/tutorial2/gamedata/modularcomms/staticcomms_mission.lua
@@ -0,0 +1,9 @@
+local comms = {
+  comm_mission_tutorial1 = {
+    chassis = "cremcom3",
+    name = "Tutorial Commander",
+    modules = { "commweapon_beamlaser", "module_autorepair", "module_autorepair", "module_ablative_armor" },
+  },
+}
+
+return comms
